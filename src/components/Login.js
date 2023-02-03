9f94c56cea3df39ce824441e4641a3e1e257e129
forrestmorrison <forrestmorrison@gmail.com>
2023-02-02 19:14:33 -0600

    added logout reducer

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,19 @@
 import { useDispatch } from "react-redux"
-import { login } from '../features/userSlice'
+import { login, logout } from '../features/userSlice'
 
 const Login = () => {
   const dispatch = useDispatch()
 
   return (
-    <button onClick={() => {
-      dispatch(login({ name: "Forrest", age: 33, email: "forrestmorrison@gmail.com"}))
-    }}>login</button>
+    <>
+      <button onClick={() => {
+        dispatch(login({ name: "Forrest", age: 33, email: "forrestmorrison@gmail.com"}))
+      }}>login</button>
+
+      <button onClick={() => {
+        dispatch(logout())
+      }}>logout</button>
+    </>
   )
 }
 
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,21 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialStateValue = { name: "", age: null, email: "" }
+
 export const userSlice = createSlice({
     name: "user",
     initialState: {
-        value: {
-            name: "",
-            age: null,
-            email: ""
-        }
+        value: initialStateValue
     },
     reducers: {
         login: (state, action) => {
             state.value = action.payload
+        },
+        logout: (state, action) => {
+            state.value = initialStateValue
         }
     }
 })
 
-export const { login } = userSlice.actions
+export const { login, logout } = userSlice.actions
 
 export default userSlice.reducer
