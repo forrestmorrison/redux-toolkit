ed298e9e0daae512198a6ab429ce6b8c7171b44f
forrestmorrison <forrestmorrison@gmail.com>
2023-02-02 18:42:25 -0600

    added dispatch function to Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,13 @@
-
+import { useDispatch } from "react-redux"
+import { login } from '../features/userSlice'
 
 const Login = () => {
+  const dispatch = useDispatch()
+
   return (
-    <button>login</button>
+    <button onClick={() => {
+      dispatch(login({ name: "Forrest", age: 33, email: "forrestmorrison@gmail.com"}))
+    }}>login</button>
   )
 }
 
